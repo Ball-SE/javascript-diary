20d939dddbdd7fd4ebe60680a81e55a2d635a5da
Pharnuwat <se56160411@gmail.com>
2025-09-07 21:50:06 +0700

    fix: path in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import SignUp from './pages/SignUpPage'
-import LogInPage from './pages/LogInPage'
-import ViewPosts from './pages/ViewPosts'
-import PageNotFound from './pages/PageNotFound'
+import HomePage from './pages/HomePage.jsx'
+import SignUp from './pages/SignUpPage.jsx'
+import LogInPage from './pages/LogInPage.jsx'
+import ViewPosts from './pages/ViewPosts.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
 function App() {
 
