abd130f50eed50dc4f61654ac655524b855143b0
Pharnuwat <se56160411@gmail.com>
2025-10-21 16:09:27 +0700

    feat: update CORS configuration to allow access from Vercel deployment

--- server/app.mjs
+++ server/app.mjs
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4001;
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://javascript-diary.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
